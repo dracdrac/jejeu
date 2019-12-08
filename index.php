2778af14c990f2f55549ed53ed7399868af1e6c6
Léon Lenclos <leon.lenclos@gmail.com>
2019-12-08 22:44:05 +0100

    fix lien modifier de index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     // PAGE : ACCUEIL
     require('template/header.php');
     echo affiche_page_article(2);
-    $lien_modifier = generer_lien_modifier('etiquettes', 2);
+    $lien_modifier = generer_lien_modifier('articles', 2);
+
     require('template/footer.php');
 ?>
 
